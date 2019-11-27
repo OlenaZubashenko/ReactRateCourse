1ed9b9053ce5c638cc278f4a0485d3b6c1ebff49
olenazubashenko <olenazubashenko@gmail.com>
2019-11-27 13:47:36 +0200

    state jn btn like

--- src/components/CurrentList/current-list.js
+++ src/components/CurrentList/current-list.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './current-list.css';
 
-const CurrentList = ({ ccy, buy, sale }) => {
-    return (<div className='list-wrapper'>
-        
+export default class CurrentList extends Component {
+    render() {
+        const { ccy, buy, sale } = this.props;
+    
+    return(
+        <div className = "list-wrapper">
         <ul>
             <li>
-            <button type="button" className="btn btn-outline-warning btn-like"
-            onClick={ ()=> console.log('try')}
-            >like</button>
-                {ccy} 
+                <button type="button" className="btn btn-outline-warning btn-like"
+                    onClick={() => console.log(`try ${ccy}, ${buy}`)}
+                >like</button>
+                {ccy}
             </li>
             <li>{buy}</li>
             <li>{sale}</li>
         </ul>
     </div>
     )
-
+};
 };
 
-export default CurrentList;
+
